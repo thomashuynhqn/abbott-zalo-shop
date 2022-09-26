d77c61ee1e9380b3a9dc920a2497d1416d4a4eee
Huynh Luong Nhut Toan <luongnhuttoan.huynh@concentrix.com>
2022-09-26 15:07:41 +0700

    new change + filter

--- src/components/inquiry.jsx
+++ src/components/inquiry.jsx
@@ -1,84 +1,54 @@
 import React, { useState, useMemo, useEffect } from "react";
-import { Box, Input, Button, Icon, useStore } from "zmp-framework/react";
+import { Box, Input, Button, Icon, Col, useStore } from "zmp-framework/react";
 import ProductList from "./product-list";
 import store from "../store";
 import "../css/inquiry.scss";
 
 const Inquiry = () => {
   const itemApi = useStore("products");
-  const [q, setQ] = useState("");
-  const [searchParam] = useState(["_id"]);
-  const [filterParam, setFilterParam] = useState(["category"]);
+  const [itemList, setItemList] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState();
 
-  // const [productList, setProductList] = useState([]);
+  useEffect(() => {
+    setItemList(itemApi);
+  });
 
   useEffect(() => {
     store.dispatch("fetchProducts");
   }, []);
 
-  function search(itemApi) {
-    return itemApi.filter((item) => {
-      /*
- // in here we check if our region is equal to our c state
- // if it's equal to then only return the items that match
- // if not return All the countries
- */
-      if (item.category == filterParam) {
-        return searchParam.some((newItem) => {
-          return (
-            item[newItem].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
-          );
-        });
-      } else if (filterParam == "category") {
-        return searchParam.some((newItem) => {
-          return (
-            item[newItem].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
-          );
-        });
-      }
-    });
+  function getFilteredList() {
+    if (!selectedCategory) {
+      return itemList;
+    }
+    return itemList.filter((product) => product.category === selectedCategory);
   }
 
-  // const filterByBrand = (filteredData) => {
-  //   if (!selectedCategory) {
-  //     return filteredData;
-  //   }
-  //   const filteredProducts = filteredData.filter(
-  //     (product) => product.products === selectedCategory
-  //   );
-  //   return filteredProducts;
-  // };
+  const filteredList = useMemo(getFilteredList, [selectedCategory, itemList]);
 
-  // const handleBrandChange = (event) => {
-  //   setSelectedCategory(event.target.value);
-  // };
-
-  // useEffect(() => {
-  //   let filteredData = filterByBrand("products");
-  //   setProductList(filteredData);
-  // }, [selectedCategory]);
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
 
   return (
     <Box className="inquiry" px="2">
       <div className="flex-1 pr-4">
-        <Input
-          type="select"
-          onChange={(e) => {
-            setFilterParam(e.target.value);
-          }}
+        <select
+          name="category-list"
+          id="category-list"
+          onChange={handleCategoryChange}
         >
-          {itemApi.map((item) => (
-            <option key={item._id} value={item.category}>
-              {item._id}
-            </option>
-          ))}
-        </Input>
+          <option value="">All</option>
+          <option value="Ensure Gold">Ensure Gold</option>
+          <option value="Glucerna">Glucerna</option>
+          <option value="PediaSure">PediaSure</option>
+        </select>
       </div>
-      <div className="product-row">
-        {search(itemApi).map((element) => (
-          <ProductList key={element.category} />
-        ))}
-      </div>
+      {filteredList.map((element, product) => (
+        <div key={product} className="product-row">
+          <ProductList {...element} />
+        </div>
+      ))}
     </Box>
   );
 };
